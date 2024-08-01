2fe739b719d625433e7ebdf075e1c8d545c06bf4
Thaboteebo <Thaboteebo01@gmail.com>
2024-08-01 14:13:27 +0000

    Python - Variable Annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Module documentation """
+
+
+def add(a: float, b: float) -> float:
+    """func doc"""
+    return a + b
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module documentation """
+import math
+
+
+def floor(n: float) -> int:
+    """func doc"""
+    return math.floor(n)
