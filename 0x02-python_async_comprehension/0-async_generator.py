6268b00cd0b4b0333f0d575180332da6f0e3f073
Thaboteebo <Thaboteebo01@gmail.com>
2024-08-06 13:32:21 +0000

    Python - Async Comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """func doc"""
+    """Func doc"""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
