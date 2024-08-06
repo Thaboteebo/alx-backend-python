2bc17b9ac713c5dcf687c70438be43c4ab8fa139
Thaboteebo <Thaboteebo01@gmail.com>
2024-08-06 17:02:21 +0000

    0x02. Python - Async Comprehension

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -7,9 +7,10 @@ async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
 async def measure_runtime() -> float:
+    """ Function documentation """
     start = time.perf_counter()
     await asyncio.gather(async_comprehension(), async_comprehension(),
                         async_comprehension(), async_comprehension()
-                        )
+                        for _ in range(10))
     end = time.perf_counter()
     return end - start
