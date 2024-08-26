d5d4a06a3816da5b3ea93b34e6246bf1909ad066
thaboteebo <Thaboteebo01@gmail.com>
2024-08-26 20:56:42 +0000

    Unittests and Integration Tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,7 +14,7 @@ class TestAccessNestedMap(unittest.TestCase):
         [
             ({"a": 1}, ("a",), 1),
             ({"a": {"b": 2}}, ("a",) {"b": 2}),
-            ({"a": {"b": 2}}, ("a", "b"), 2),
+            ({"a": {"b": 2}}, ("a", "b"), 2)
         ]
     )
     def test_access_nested_map(
