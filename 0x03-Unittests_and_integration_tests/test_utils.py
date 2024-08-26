9f13279594c0392635b35609c552115c4ddfc33d
thaboteebo <Thaboteebo01@gmail.com>
2024-08-26 10:46:43 +0000

    Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -49,3 +49,27 @@ class TestGetJson(unittest.TestCase):
         mock_get.return_value.json.return_value = test_payload
         self.assertEqual(get_json(test_url), test_payload)
         mock_get.assert_called_once_with(test_url)
+
+class TestMemoize(unittest.TestCase):
+    """doc doc doc"""
+
+    def test_memoize(self) -> None:
+        """doc doc doc"""
+
+        class TestCase:
+            """doc doc doc"""
+
+            def a_method(self) -> int:
+                """doc doc doc"""
+                return 42
+
+            @memoize
+            def a_property(self) -> int:
+                """doc doc doc"""
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method", return_value=42) as mocked:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property, 42)
+            self.assertEqual(test_class.a_property, 42)
+            mocked.assert_called_once()
