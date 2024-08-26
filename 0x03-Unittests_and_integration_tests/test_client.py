8a2b1c5412b50ad54bbc8f92bc882c2456835363
thaboteebo <Thaboteebo01@gmail.com>
2024-08-26 11:13:31 +0000

    Mocking property

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,25 @@
+#!/usr//bin/env python3
+""" doc doc doc """
+from unittest.mock import patch, Mock, PropertyMock
+from parameterized import parameterized, parameterized_class
+from client import GithibOrgClient
+import unittest
+from fixtures import TEST_PAYLOAD
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """doc doc doc"""
+
+    @parameterized.expand(
+        [
+            ("google"),
+            ("abc"),
+        ]
+    )
+    @patch("client.get_json", return_value={"payload": True})
+    def test_org(self, org_name: str, mock_get: Mock) -> None:
+        """doc doc doc"""
+        github_org_client = GithubOrgClient(org_name)
+        self.assertEqual(github_org_client.org, {"payload": True})
+        url = f"https://api.github.com/orgs/{org_name}"
+        mock_get.assert_called_once_with(url)
